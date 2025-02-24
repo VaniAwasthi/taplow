d6bde8b06e008368aa2590202019561393645c65
Vani Awasthi <awasthivani95@gmail.com>
2025-02-24 12:45:17 +0530

    topnav-change

--- app/services/page.js
+++ app/services/page.js
@@ -4,7 +4,7 @@ import React from "react";
 const Services = () => {
   return (
     <div className="container mx-auto px-6 py-12">
-      <h1 className="text-4xl font-bold text-center">About Us</h1>
+      <h1 className="text-4xl font-bold text-center">Services</h1>
       <p className="mt-4 text-lg text-center text-gray-600">
         Welcome to our Services page. Learn more about our Services .
       </p>
