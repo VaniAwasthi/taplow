80d92daba5e86279536335b5f86527b74d4278db
Vani Awasthi <awasthivani95@gmail.com>
2025-02-25 12:47:02 +0530

    changes

--- app/services/page.js
+++ app/services/page.js
@@ -1,7 +1,7 @@
 "use-client";
 import Image from "next/image";
 import React from "react";
-import ServicePage from "../Assest/homePage/service.jpg";
+import ServicePage from "../assets/homePage/service.jpg";
 
 const Services = () => {
   return <Image src={ServicePage} alt="Service" className="w-full" />;
