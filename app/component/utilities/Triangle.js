c5121e6a6cd051acd27db5eb0c281377bae5b6c2
Vani Awasthi <awasthivani95@gmail.com>
2025-03-03 17:56:46 +0530

    triangle-changes

--- app/component/utilities/Triangle.js
+++ app/component/utilities/Triangle.js
@@ -11,29 +11,29 @@ const TriangleSection = () => {
   return (
     <>
       <div className="relative w-[650px] hidden lg:flex items-center justify-center mt-[6rem] mb-[5rem] ">
-        <div className="h-32  min-w-100%  relative translate-x-[149px] translate-y-[-40px] xl:translate-x-[175px] xl:translate-y-[-62px]">
+        <div className="z-50 h-32  min-w-100%  relative translate-x-[149px] translate-y-[-40px] xl:translate-x-[175px] xl:translate-y-[-62px]">
           <Image
-            className="object-contain transition-transform duration-300 ease-in-out hover:scale-105"
+            className="z-50 object-contain transition-transform duration-300 ease-in-out hover:scale-105"
             src={GrowthImg}
             alt="Growth"
           />
         </div>
         <div className="flex ">
-          <div className="h-32 min-w-100% relative translate-x-[-57px] translate-y-[77px] xl:translate-x-[-62px] xl:translate-y-[72px]">
+          <div className="z-10  h-32 min-w-100% relative translate-x-[-57px] translate-y-[77px] xl:translate-x-[-62px] xl:translate-y-[72px]">
             <Image
               className="object-contain transition-transform duration-300 ease-in-out hover:scale-105 "
               src={PerformanceImg}
               alt="Performance"
             />
           </div>
-          <div className="h-32 min-w-100% relative translate-x-[-127px] translate-y-[76px] xl:translate-x-[-143px] xl:translate-y-[72px]">
+          <div className="z-10  h-32 min-w-100% relative translate-x-[-127px] translate-y-[76px] xl:translate-x-[-143px] xl:translate-y-[72px]">
             <Image
               className="object-contain scale-105 transition-transform duration-200 ease-in-out hover:scale-110"
               src={AccelerateImg}
               alt="Accelerate"
             />
           </div>
-          <div className="h-32  min-w-100% relative translate-x-[-197px] translate-y-[78px] xl:translate-x-[-223px] xl:translate-y-[72px]">
+          <div className="z-10  h-32  min-w-100% relative translate-x-[-197px] translate-y-[78px] xl:translate-x-[-223px] xl:translate-y-[72px]">
             <Image
               className="object-contain transition-transform duration-300 ease-in-out hover:scale-105"
               src={DiversificationImg}
