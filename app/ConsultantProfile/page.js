461577c8f8c09d2fb12a4b617ac954c7c1260aba
Vani Awasthi <awasthivani95@gmail.com>
2025-03-22 17:46:49 +0530

    consultantPage

--- app/ConsultantProfile/page.js
+++ app/ConsultantProfile/page.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Suspense } from "react";
+
 import ConsultantProfile from "../component/consultantDirectory/ConsultantProfile";
 
 const page = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <ConsultantProfile />
-    </>
+    </Suspense>
   );
 };
 
