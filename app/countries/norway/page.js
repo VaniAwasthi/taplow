948e1fc8ba52ceb0b771d858727d9689a674c209
Vani Awasthi <awasthivani95@gmail.com>
2025-04-25 12:51:05 +0530

    norway-countrypage and austrilia new photo and sangeeta sabharwal data

--- app/countries/norway/page.js
+++ app/countries/norway/page.js
@@ -2,14 +2,13 @@
 import React from "react";
 import Banner from "../../assets/globalpage/norwayBanner.webp";
 import {
-  GlobalLocation,
   GlobalpageBanner,
   SuccessStory,
 } from "@/app/component/coun-layout/GlobalpageBanner";
 import OneOffice from "@/app/component/coun-layout/OneOffice";
-import officeImage from "../../assets/globalpage/AustraliaOffice.webp";
-import DrivingLeadership from "@/app/component/coun-layout/DrivingLeadership";
-import Australiaflag from "../../assets/globalpage/australiaflag.webp";
+import officeImage from "../../assets/globalpage/Norwayoffice.webp";
+import generalImg from "../../assets/globalpage/generalImg.webp";
+import Norwayflag from "../../assets/globalpage/norwayflag.webp";
 import DiversityImg from "../../assets/globalpage/diversity.webp";
 import IndustrySlider from "@/app/component/home/Industries";
 import { TeamSection } from "@/app/component/coun-layout/SweadenTeamsec";
@@ -17,6 +16,7 @@ import Ethical1 from "../../assets/aboutuspage/Ethical1.webp";
 import Ethical2 from "../../assets/aboutuspage/Ethical2.webp";
 import Ethical3 from "../../assets/aboutuspage/Ethical3.webp";
 import Ethical4 from "../../assets/aboutuspage/Ethical4.webp";
+import TaplowInfoSection from "@/app/component/coun-layout/TaplowInfoSection";
 const page = () => {
   const BannerData = {
     title: `Welcome to <b>Taplow Norway</b>`,
@@ -26,25 +26,57 @@ const page = () => {
   const officeData = {
     id: 1,
     image: officeImage,
-    name: "Melbourne",
-    address:
-      "Melbourne Quarter, Building Two, Level 3, #309, 697 Collins Street, Melbourne, Victoria, Australia, 3008.",
-    phone: "+44(0)1442 383361",
+    name: "Oslo",
+    address: "Henrik Ibsens gate 90, 0255 Oslo, Norway",
+    phone: "604-687-6464",
     email: "eric@macdonaldsearchgroup.com",
   };
-
+  const greenDescriptionBox =
+    "Most of our clients are Norwegian companies, but we cooperate with other offices in the Taplow Group, primarely in the Nordic region. We like to follow our customers out in the world. Our prime focus is on management positions, specialists, and other key personel. ";
   const AboutContent = {
-    greenDescription: `Taplow Australia is set apart from its competitors by our longstanding business relationships that have been developed over more than 25 years and provide to both clients and candidates alike access to a significant pool of talent and employers of choice.`,
-    Aboutheading:
-      "Taplow Australia: Driving Leadership and Diversity Excellence",
-    aboutContentpara1:
-      "We hold in the highest esteem the values of trust, quality, professionalism, integrity, honesty and commitment to customer service, and these are clearly apparent in the work we do on a day-to-day basis.",
-    aboutContentpara2:
-      "Our industry knowledge, gathered over more than 25 years, and our candidate networks, are second to none, ensuring the right candidate fit, first time every time.",
-    aboutContentpara3:
-      "Taplow Australia is set apart from its competitors by our longstanding business relationships that have been developed over more than 25 years and provide to both clients and candidates alike access to a significant pool of talent and employers of choice.",
-    aboutContentpara4:
-      "The Taplow Group was established in Australia in 2002 and brings to clients both traditional and contemporary recruitment methodologies. Our focus is on delivering a service that identifies the best talent for your business.",
+    general: {
+      title: "General Information",
+      paragraphs: [
+        "Our experienced consultants have former practice from leading Norwegian and International companies, and are thus able to advice our clients beyond the ordinary recruitment process.The consultants keep up to date in their focus-industries to be able to find the best candidates, give the employers feedback from the market, and build a strong network",
+        "We know the companies in our focus-industries, and we understand the positioning among the companies. This is important in a fast-changing market were technology effect the way we do business, the organization of the companies, and the positions to be filled.",
+        "Taplow Norway apply well targeted processes to find the best candidate in every search we undertake. We still work hard to improve - and make sure that every candidate has a positive and learning experience, weather employed by our client or not. The Assignment shall be a positive experience to the employer as well – a professional and effective process, and good “Employer Branding”. ",
+        "We aim on recruiting the best candidate for the position, and that the new hire stays in the company for years.  We succeed well – and we deliver our assignments with a guarantee.",
+      ],
+      image: DiversityImg,
+    },
+    specific: {
+      title: "Specific Information",
+      heading: "Taplow Norway: Vision & Mission",
+      image: generalImg,
+      services: [
+        {
+          title: "VALUES",
+          content: `<p>To be able to comply by our main values; Professionality, Integrity and Delivery Excellence, we focus on industries where we have extensive experience and competence. Taplow Norway have consultants with experience from several </p>`,
+        },
+
+        {
+          title: "TAILORED RECRUITING PROCESSES",
+          content: `<p>Taplow Norway focus on Executive Search because this method is the safest way to deliver good results. However, there might be cases where advertising can be a better choice, and the two methods can supplement each other.</p>`,
+        },
+        {
+          title: "EXPERTISE",
+          content: `<ul class="list-disc list-inside space-y-1">
+            <li>Retail – Consumer Products</li>
+            <li>E-commerce</li>
+            <li>ICT</li>
+            <li>Financial Services</li>
+            <li>Financial Services</li>
+            <li>Real Estate</li>
+            <li>Industrial and Energy</li>
+            <li>Construction</li>
+            </ul>`,
+        },
+        {
+          title: "",
+          content: `<p>We have the professional setup to do the entire hiring process. Some of our customers prefer to do parts of the process themselves. Taplow Norway tailor the process so the customers get what they want. <b>We deliver our assignments with a guarantee.</b></p> `,
+        },
+      ],
+    },
   };
   const SuccessStoryslidesData = [
     {
@@ -80,24 +112,20 @@ const page = () => {
         desc={BannerData.desc}
         Banner={Banner}
       />
-      {/* <OneOffice officeData={officeData} heading={officeSecHeading} />
-      <DrivingLeadership
-        greenDescription={AboutContent.greenDescription}
-        Aboutheading={AboutContent.Aboutheading}
-        flag={Australiaflag}
-        aboutContentpara1={AboutContent.aboutContentpara1}
-        aboutContentpara2={AboutContent.aboutContentpara2}
-        aboutContentpara3={AboutContent.aboutContentpara3}
-        aboutContentpara4={AboutContent.aboutContentpara4}
-        ColImg={DiversityImg}
+      <OneOffice officeData={officeData} heading={officeSecHeading} />
+      <TaplowInfoSection
+        {...AboutContent}
+        greenDescription={greenDescriptionBox}
+        flag={Norwayflag}
+        sepecificationHeading="md:!w-[250px]"
       />
       <IndustrySlider />
-      <TeamSection country="Australia" />
+      <TeamSection country="Norway" />
       <SuccessStory
         SuccessData={SuccessStoryslidesData}
         heading={Successheading}
       />
-      <GlobalLocation /> */}
+      {/* <GlobalLocation /> */}
     </>
   );
 };
