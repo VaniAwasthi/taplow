906434952e14dbf7c63d38ae1c383d3c5a07bad8
Vani Awasthi <awasthivani95@gmail.com>
2025-05-26 12:43:28 +0530

    Entrim management page

--- app/services/board-advisory-services/page.js
+++ app/services/board-advisory-services/page.js
@@ -7,11 +7,18 @@ import {
   WhyChooseBoardAdvisory,
 } from "@/app/component/boardadvisory/HeroAboutWhy";
 import IndustrySlider from "@/app/component/home/Industries";
+import ContcatSection from "@/app/component/services/ContactSection";
 import Faq from "@/app/component/services/Faq";
 import MeetOurExperts from "@/app/component/services/MeetOurExpert";
 import React from "react";
 
 const Page = () => {
+  const ContactData = {
+    heading: "Connect with a Board Advisory Expert",
+
+    description:
+      "Ready to find your next great leader? Fill out the form below to Connect with a Board Advisory Expert and discuss your hiring needs.",
+  };
   return (
     <>
       <HeroSection />
@@ -22,6 +29,7 @@ const Page = () => {
       <MeetOurExperts />
       <SuccesStoryBoard />
       <Faq />
+      <ContcatSection ContactData={ContactData} />
     </>
   );
 };
