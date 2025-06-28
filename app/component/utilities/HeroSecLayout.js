0e6670b3e1537a734dfc66cb41afd085bb310148
Vani Awasthi <awasthivani95@gmail.com>
2025-06-28 17:46:49 +0530

    newzeland

--- app/component/utilities/HeroSecLayout.js
+++ app/component/utilities/HeroSecLayout.js
@@ -45,7 +45,7 @@ const HeroSection = ({
         {/* Left Side - Text */}
         <div className={`text-white md:w-1/2 text-left  ${leftSecClass}`}>
           <h2
-            style={{ lineHeight: "2.5rem" }}
+            style={{ lineHeight: "3.5rem" }}
             className={`text-2xl md:text-4xl font-normal text-left ${headingClass}`}
             dangerouslySetInnerHTML={{ __html: Heading }}
           ></h2>
